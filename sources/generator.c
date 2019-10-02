759bdbe812f544134176e95fd8be1aa7f933df84
fatrujilloa <fatrujilloa@gmail.com>
2019-10-02 23:08:08 +0200

    generator

---
+++ sources/generator.c
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   generateur.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ftrujill <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/02 21:02:38 by ftrujill          #+#    #+#             */
+/*   Updated: 2019/10/02 23:01:41 by ftrujill         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <time.h> 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "../libft/libft.h"
+
+int     print_usage()
+{
+    ft_printf("The generator takes none or exactly three arguments as input "
+        "corresponding to the length (4 to 50), width (4 to 100) and name of "
+        "the map. If the numeric values do not belong to this range they are "
+        "automatically adjusted.\n");
+    return (0);
+}
+
+int     rand_range(int lower, int upper)
+{
+    return (lower > upper ? 0 : rand() % (upper - lower + 1) + lower);
+}
+
+void    random_positions(int x, int y, int p[4])
+{
+    p[0] = rand_range(1, x);
+    p[1] = rand_range(1, y);
+    p[2] = rand_range(1, x);
+    p[3] = p[1];
+    while (p[3] == p[1])
+        p[3] = rand_range(1, y);
+}
+
+void    create_map(int x, int y, const char *name)
+{
+    int p[4];
+    int i;
+    int j;
+    int fd;
+
+    if((fd = open(name, O_CREAT | O_RDWR)) < 0)
+        return ;
+    random_positions(x, y, p);
+    i = 0;
+    while (i++ < x)
+    {
+        j = 0;
+        while (j++ < y)
+        {
+            if (i == p[0] && j == p[1])
+                write (fd, "O", 1);
+            else if (i == p[2] && j == p[3])
+                write(fd, "X", 1);
+            else
+             write(fd, ".", 1);
+        }
+        write(fd, "\n", 1);
+    }
+    close (fd);
+}
+
+int     main(int argc, char **argv)
+{
+    int x;
+    int y;
+
+    srand(time(0));
+    if (argc == 1)
+        {
+            create_map(rand_range(4, 50), rand_range(4, 100), "map");
+            return (0);
+        }
+    if (argc != 4)
+        return (print_usage());
+    x = ft_min_int(ft_atoi(argv[1]), 50);
+    y = ft_min_int(ft_atoi(argv[2]), 100);
+    create_map(ft_max_int(4, x), ft_max_int(4, y), argv[3]);
+}
